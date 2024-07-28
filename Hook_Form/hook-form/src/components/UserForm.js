155d51d3e974e3ca8c99ec8da128c7c64372599e
Sarra Ben Brahim <sarra-ben-brahim@users.noreply.github.com>
2024-07-28 12:30:45 +0100

    Hook Form Practice

---
+++ Hook_Form/hook-form/src/components/UserForm.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+const UserForm = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+            confirmPassword: confirmPassword
+        };
+        console.log('User Data:', formData);
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>First Name: </label>
+                    <input type="text" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                </div>
+                <div>
+                    <label>Last Name: </label>
+                    <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                </div>
+                <div>
+                    <label>Email: </label>
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                </div>
+                <div>
+                    <label>Password: </label>
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                <div>
+                    <label>Confirm Password: </label>
+                    <input type="password" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
+            <div>
+                <h2>Your Form Data</h2>
+                <p>First Name: {firstName}</p>
+                <p>Last Name: {lastName}</p>
+                <p>Email: {email}</p>
+                <p>Password: {password}</p>
+                <p>Confirm Password: {confirmPassword}</p>
+            </div>
+        </div>
+    )
+}
+
+export default UserForm
